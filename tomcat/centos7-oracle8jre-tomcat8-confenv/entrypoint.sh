4b92371b2544a1593a144bd6a0eee53cef81ff14
uname.chen <uname.chen@gmail.com>
2017-08-01 14:34:37 +0800

    update script for tomcat8+configenv

--- tomcat/centos7-oracle8jre-tomcat8-confenv/entrypoint.sh
+++ tomcat/centos7-oracle8jre-tomcat8-confenv/entrypoint.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
 ## variable define
-DISCONF_PATH = '/opt/webapp/WEB-INF/classes/disconf.properties'
+DISCONF_PATH='/opt/webapp/WEB-INF/classes/disconf.properties'
+if [ $CONFIG_FILE ];then
+    echo "DISCONF_PATH = $CONFIG_FILE"
+fi
 
 ## disconf ENV settings
 # disconf host
 if [ -z $CONFIG_H ];then
    cat $DISCONF_PATH | grep 'conf_server_host='
 else
+    echo "user define conf_server_host: "$CONFIG_H
     sed -i "s/conf_server_host=.*/conf_server_host=$CONFIG_H/g" $DISCONF_PATH
 fi
 
@@ -15,6 +19,7 @@ fi
 if [ -z $CONFIG_V ];then
    cat $DISCONF_PATH | grep 'version='
 else
+    echo "user define version: "$CONFIG_V
     sed -i "s/version=.*/version=$CONFIG_V/g" $DISCONF_PATH
 fi
 
@@ -22,6 +27,7 @@ fi
 if [ -z $CONFIG_APP ];then
    cat $DISCONF_PATH | grep 'app='
 else
+    echo "user define app: "$CONFIG_APP
     sed -i "s/app=.*/app=$CONFIG_APP/g" $DISCONF_PATH
 fi
 
@@ -29,6 +35,7 @@ fi
 if [ -z $CONFIG_DIR ];then
    cat $DISCONF_PATH | grep 'user_define_download_dir='
 else
+    echo "user define user_define_download_dir: "$CONFIG_DIR
     sed -i "s/user_define_download_dir=.*/user_define_download_dir=$CONFIG_DIR/g" $DISCONF_PATH
 fi
 
